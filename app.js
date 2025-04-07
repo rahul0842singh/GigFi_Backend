0deff8fd54b066053fdcb31c5d47323ddd364c34
rahul0842singh <42551992+rahul0842singh@users.noreply.github.com>
2025-04-07 22:35:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -899,8 +899,8 @@ app.post('/api/transaction', authenticateToken, (req, res) => {
 app.post('/api/walletconnectinsert', (req, res) => {
   const {  walletaddress } = req.body;
 
-  if (!wallet_address) {
-    return res.status(400).json({ error: 'id and wallet_address are required' });
+  if (!walletaddress) {
+    return res.status(400).json({ error: 'id and walletaddress are required' });
   }
 
   const sql = 'INSERT INTO walletconnect (walletaddress) VALUES ( ?)';

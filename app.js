4159284c3fa1c05726aca0efe380c78d38018d0c
rahul0842singh <42551992+rahul0842singh@users.noreply.github.com>
2025-04-11 22:26:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -345,7 +345,7 @@ app.get('/api/postings', authenticateToken, (req, res) => {
 
 
 // Create a new chatroom with an optional display picture
-app.post('/api/chatrooms', authenticateToken, upload.single('display_picture'), (req, res) => {
+app.post('/api/chatrooms/:created_by', authenticateToken, upload.single('display_picture'), (req, res) => {
   const { name, details } = req.body;
   const createdBy = req.user.id;
 
